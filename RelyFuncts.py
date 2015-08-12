e3094002fb060c67f75932614e7dff64b20be354
Mark Kampe <mark.kampe@gmail.com>
2015-08-11 17:31:43 -0700

    RelyFuncts: added new parameter to all_fail 	does each failure start a new repair period

--- RelyFuncts.py
+++ RelyFuncts.py
@@ -73,28 +73,33 @@ def Pn(expected=1, n=0):
     return p
 
 
-def allFail(fitRate, total, required, repair):
+def allFail(fitRate, total, required, repair, oneRepair=True):
     """ effective FIT rate required/total redundant components
             fitRate -- FIT rate of a single component
             total -- number of redundant components in system
             required -- number required for continued operation
             repair -- repair time (in hours)
+            oneRepair -- all failures within a single repair period
+
+        FITs(all_fail) =
+            FITs(initial failure) * P(rest fail during repair period)
     """
 
-    # FIX
-    #   I could not readily find this formula, and needed one,
-    #   so I made this up.  I should do some research and use
-    #   a correct one.
-    #
-    #   start with the FITs of the initial set of components
-    #   for each remaining redundant component
-    #       multiply by probability of next faiulre (within repair window)
-    #
+    # FIX ... these are only approximations, I should do better
     fits = total * fitRate      # initial FIT rate
-    total -= 1
-    while total >= required:    # probability of succesive failures
-        P_nextfail = total * fitRate * repair * 10E-9
-        fits *= P_nextfail
-        total -= 1
-
+    total -= 1                  # we are down one
+    if oneRepair:
+        # all failures must occur within a single repair period
+        # note: this very slightly under-estimates P_restfail in cases
+        #       where required > 1
+        P_restfail = Pfail(total*fitRate, repair, n=total+1-required)
+        fits *= P_restfail
+    else:
+        # each failure starts a new repair period
+        # note: these numbers are small enough that expected reasonably
+        #       approximates the probability
+        while total >= required:
+            P_nextfail = total * fitRate * repair * 10E-9
+            fits *= P_nextfail
+            total -= 1
     return fits

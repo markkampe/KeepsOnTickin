a37c72c961c1184493d86485a5452f97c1484f18
Mark Kampe <mark.kampe@gmail.com>
2015-08-12 19:07:14 -0700

    reliability model:	cleanups 	factor out printParms 	support new verbosity parameter 	add primaries/secondaries to output 	separate options for one-time vs every time parms ouput 	more intelligent appending of "per year" suffix to legend

--- run.py
+++ run.py
@@ -7,25 +7,61 @@ from Model import Model, Sizes, Rates, Results
 from ColumnPrint import ColumnPrint
 
 
-def run(models, capacity=1*PiB, period=1*YEAR, repair=24*HOUR,
-        verbosity="headings"):
+def printParms(model, rates):
+    """ print out selected parameters for this test
+        model -- the model to be printed
+        rates -- the computed FIT rates
+    """
+    print ""
+    print "Parameters:"
+    dram = "%dGB DRAM, %d FITs/GB, 2-bit=%f" %\
+           (model.sz_dram, model.f_dram, model.dram_2bit)
+    nvram = "%dGB NVRAM, UBER=%e" % (model.sz_nvram, model.uer_nvm)
+    if model.n_nvram_1 > 0:
+        print("\tprimary:  \t%dx%s" % (model.n_nvram_1, nvram))
+    else:
+        print("\tprimary:  \t%dx%s" % (model.n_dram_1, dram))
+    if model.n_nvram_2 > 0:
+        print("\tsecondary:\t%dx%s" % (model.n_nvram_2, nvram))
+    elif model.n_dram_2 > 0:
+        print("\tsecondary:\t%dx%s" % (model.n_dram_2, dram))
+    print("\tcopies:    \t%d, fan-out=%d, fan-in=%d" %
+          (model.copies, model.fan_out, model.fan_in))
+    print("\tcapacity:  \tused=%d%%, active=%d%%" %
+          (model.cap_used * 100, model.lun_active * 100))
+    print("\tcache:     \tcached=%d%%, dirty=%d%%" %
+          (model.lun_cached * 100, model.lun_dirty * 100))
+    print("\tdet/recov: \t%ds, %dMB/s" %
+          (model.time_detect, model.rate_flush/1000000))
+    print("\tload:      \t%d/%dx%d byte writes/s" %
+          (model.write_iops, model.write_reduce, model.bsize))
+    print("\tsoftware:  \tFITs=%d, hard=%f" % (model.f_sw, model.sw_hard))
+
+    if rates is not None:
+        print("\tloss(1):   \tFITs=%d" % (rates.fits_1_loss))
+        print("\tloss(2):   \tFITs=%d" % (rates.fits_2_loss))
+
+
+def run(models, verbosity="default",
+        capacity=1*PiB, period=1*YEAR, repair=24*HOUR):
     """ execute a single model and print out the results
         models -- list of models to be run
+        verbosity -- what kind of output we want
         capacity -- total system capacity (bytes)
         period -- modeled time period (hours)
         repair -- modeled repair time (hours)
-        verbosity -- what kind of output we want
     """
 
     # define the column headings
-    heads = ("configuration", "durability", "PL(node)", "PL(NRE)",
-             "loss/PiB")
+    heads = ("configuration", "<p,s>/PiB", "durability",
+             "PL(node)", "PL(NRE)", "loss/PiB")
     legends = [
         "configuration being modeled",
-        "probability of object survival",
-        "probability of loss due to node failures",
-        "probability of loss due to NREs during recovery",
-        "expected data loss per Petabyte"
+        "<primaries, secondaries> per petabyte",
+        "probability of object survival*",
+        "probability of loss due to node failures*",
+        "probability of loss due to NREs during recovery*",
+        "expected data loss per petabyte*"
     ]
 
     # figure out the longest description
@@ -38,58 +74,67 @@ def run(models, capacity=1*PiB, period=1*YEAR, repair=24*HOUR,
     format = ColumnPrint(heads, maxdesc=maxlen)
 
     # figure out what output he wants
-    headings = True
-    parms = True
-    descr = True
-    debug = False
+    parm1 = True        # basic parameters at front
+    descr = True        # descriptions of columns
+    headings = True     # column headings
+    parms = True        # all parameters for every model
+    debug = False       # diagnostic output
     if verbosity == "parameters":
+        parm1 = False
         descr = False
     elif verbosity == "headings":
+        parm1 = False
         parms = False
         descr = False
-    elif verbosity == "data only":
+    elif verbosity == "data":
+        parm1 = False
         parms = False
         descr = False
         headings = False
     elif verbosity == "debug":
         debug = True
+        parm1 = False
+    else:
+        parms = False
 
+    # print out basic parameters (assumed not to change)
+    if parm1:
+        printParms(models[0], None)
+
+    # print out column legends
     if descr:
         print ""
-        print "Column legends"
+        print "Column legends:"
         s = format.printTime(period)
         i = 1
         while i <= len(legends):
             l = legends[i - 1]
-            if i == 1:
-                print "\t%d %s" % (i, l)
-            else:
+            if l.endswith('*'):
                 print "\t%d %s (per %s)" % (i, l, s)
+            else:
+                print "\t%d %s" % (i, l)
             i += 1
 
+    # print out column headings
     if headings:
         format.printHeadings()
 
     for m in models:
-
-        # run the model
-        sizes = Sizes(m, capacity)
-        rates = Rates(m, repair)
-        results = Results(m, sizes, rates, period)
+        # compute sizes and rates
+        sizes = Sizes(m, capacity, debug)
+        rates = Rates(m, repair, debug)
 
         # print out the model parameters
         if parms:
-            print "TBD"
+            printParms(m, rates)
 
-        # print out the report results
+        # compute and print the reliability
+        results = Results(m, sizes, rates, period, debug)
         s = list()
         s.append(m.descr)
+        s.append("<%d,%d>" % (sizes.n_primary, sizes.n_secondary))
         s.append(format.printDurability(results.durability))
         s.append(format.printProbability(results.p_loss_node))
         s.append(format.printProbability(results.p_loss_copy))
-
-        l = results.p_loss_node * results.exp_loss_node
-        l += results.p_loss_copy * results.exp_loss_copy
-        # FIX: l *= PiB / raw capacity
-        s.append(format.printFloat(l))
+        s.append(format.printFloat(results.exp_loss_all))
         format.printLine(s)

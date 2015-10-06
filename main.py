8ae00af6ef5c862604fd0cdd358a163b4085831c
Mark Kampe <mark.kampe@gmail.com>
2015-10-06 15:49:07 -0700

    reliability model: add symmetric tests 	update model parameters for correct symmetric tests

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ main routine for driving simulations
 
 from Model import Model
 from run import run
+from sizes import GB
 
 
 def defaultTests(verbosity="default"):
@@ -15,98 +16,132 @@ def defaultTests(verbosity="default"):
         # create a list of tests to be run
         mlist = list()
 
-        m = Model("prim: v,  no copies, 30s, 50MB/s")
-        m.nv_1 = False
+        m = Model("prim: v,  no copies, 30s, 50-100MB/s")
         m.copies = 1
+        m.cache_1 = 1 * GB
+        m.nv_1 = False
         m.time_detect = 30
         m.rate_flush = 50000000
+        m.rate_mirror = 100000000
         mlist.append(m)
 
-        m = Model("prim: nv, no copies, 30s, 50MB/s")
-        m.nv_1 = True
+        m = Model("prim: nv, no copies, 30s, 50-100MB/s")
         m.copies = 1
-        m.time_detect = 30
-        m.rate_flush = 50000000
-        mlist.append(m)
-
-        m = Model("symmetric: 2 v copy, 30s, 50MB/s")
-        m.symmetric = True
-        m.nv_1 = False
-        m.nv_2 = False
-        m.copies = 2
-        m.time_detect = 30
-        m.rate_flush = 50000000
-        mlist.append(m)
-
-        m = Model("prim: v,  1 v copy, 30s, 50MB/s")
-        m.nv_1 = False
-        m.nv_2 = False
-        m.copies = 2
-        m.time_detect = 30
-        m.rate_flush = 50000000
-        mlist.append(m)
-
-        m = Model("prim: v,  1 nv copy, 30s, 50MB/s")
-        m.nv_1 = False
-        m.nv_2 = True
-        m.copies = 2
-        m.time_detect = 30
-        m.rate_flush = 50000000
-        mlist.append(m)
-
-        m = Model("symmetric: 2 nv copy, 30s, 50MB/s")
-        m.symmetric = True
+        m.cache_1 = 1 * GB
         m.nv_1 = True
-        m.nv_2 = True
-        m.copies = 2
         m.time_detect = 30
         m.rate_flush = 50000000
+        m.rate_mirror = 100000000
         mlist.append(m)
 
-        m = Model("prim: nv, 1 nv copy, 30s, 50MB/s")
-        m.copies = 2
-        m.nv_1 = True
-        m.nv_2 = True
-        m.rate_flush = 50000000
-        mlist.append(m)
-
-        m = Model("symmetric: 3 v copy, 30s, 50MB/s")
+        m = Model("symmetric: 2 v copy, 30s, 50-100MB/s")
         m.symmetric = True
+        m.copies = 2
+        m.cache_1 = 2 * GB
         m.nv_1 = False
+        m.cache_2 = 0
         m.nv_2 = False
-        m.copies = 3
         m.time_detect = 30
         m.rate_flush = 50000000
+        m.rate_mirror = 100000000
         mlist.append(m)
 
-        m = Model("prim: v,  2 v copy, 30s, 50MB/s")
-        m.copies = 3
+        m = Model("prim: v,  1 v copy, 30s, 50-100MB/s")
+        m.copies = 2
+        m.cache_1 = 1 * GB
         m.nv_1 = False
+        m.cache_2 = 40 * GB
         m.nv_2 = False
+        m.time_detect = 30
         m.rate_flush = 50000000
+        m.rate_mirror = 100000000
         mlist.append(m)
 
-        m = Model("prim: v,  2 nv copy, 30s, 50MB/s")
-        m.copies = 3
+        m = Model("prim: v,  1 nv copy, 30s, 50-100MB/s")
+        m.copies = 2
+        m.cache_1 = 1 * GB
         m.nv_1 = False
+        m.cache_2 = 40 * GB
         m.nv_2 = True
+        m.time_detect = 30
         m.rate_flush = 50000000
+        m.rate_mirror = 100000000
         mlist.append(m)
 
-        m = Model("symmetric: 3 nv copy, 30s, 50MB/s")
+        m = Model("symmetric: 2 nv copy, 30s, 50-100MB/s")
         m.symmetric = True
+        m.copies = 2
+        m.cache_1 = 2 * GB
         m.nv_1 = True
+        m.cache_2 = 0
         m.nv_2 = True
-        m.copies = 3
         m.time_detect = 30
         m.rate_flush = 50000000
+        m.rate_mirror = 100000000
         mlist.append(m)
 
-        m = Model("prim: nv, 2 nv copy, 30s, 50MB/s")
-        m.copies = 3
+        m = Model("prim: nv, 1 nv copy, 30s, 50-100MB/s")
+        m.copies = 2
+        m.cache_1 = 1 * GB
         m.nv_1 = True
+        m.cache_2 = 40 * GB
         m.nv_2 = True
         m.rate_flush = 50000000
+        m.rate_mirror = 100000000
+        mlist.append(m)
+
+        m = Model("symmetric: 3 v copy, 30s, 50-100MB/s")
+        m.symmetric = True
+        m.copies = 3
+        m.cache_1 = 3 * GB
+        m.nv_1 = False
+        m.cache_2 = 0
+        m.nv_2 = False
+        m.time_detect = 30
+        m.rate_flush = 50000000
+        m.rate_mirror = 100000000
+        mlist.append(m)
+
+        m = Model("prim: v,  2 v copy, 30s, 50-100MB/s")
+        m.copies = 3
+        m.cache_1 = 1 * GB
+        m.nv_1 = False
+        m.cache_2 = 40 * GB
+        m.nv_2 = False
+        m.rate_flush = 50000000
+        m.rate_mirror = 100000000
+        mlist.append(m)
+
+        m = Model("prim: v,  2 nv copy, 30s, 50-100MB/s")
+        m.copies = 3
+        m.cache_1 = 1 * GB
+        m.nv_1 = False
+        m.cache_2 = 40 * GB
+        m.nv_2 = True
+        m.rate_flush = 50000000
+        m.rate_mirror = 100000000
+        mlist.append(m)
+
+        m = Model("symmetric: 3 nv copy, 30s, 50-100MB/s")
+        m.copies = 3
+        m.symmetric = True
+        m.cache_1 = 3 * GB
+        m.nv_1 = True
+        m.cache_2 = 0
+        m.nv_2 = True
+        m.time_detect = 30
+        m.rate_flush = 50000000
+        m.rate_mirror = 100000000
+        mlist.append(m)
+
+        m = Model("prim: nv, 2 nv copy, 30s, 50-100MB/s")
+        m.copies = 3
+        m.cache_1 = 1 * GB
+        m.nv_1 = True
+        m.cache_2 = 40 * GB
+        m.nv_2 = True
+        m.rate_flush = 50000000
+        m.rate_mirror = 100000000
         mlist.append(m)
 
         # run all the specified models
@@ -122,7 +157,7 @@ def main():
     parser.add_option("-g", "--gui", dest="gui", action="store_true",
                       default=False, help="GUI control panel")
     parser.add_option("-v", "--verbosity", dest="verbose",
-                      metavar="data|headings|parameters|debug|trace",
+                      metavar="data|headings|parameters|debug|all",
                       default="")
     (opts, files) = parser.parse_args()
 
